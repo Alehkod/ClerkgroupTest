c7d3191d594d1e305c3007613530bcc4a7c2369a
alehkod <kodikoleg@gmail.com>
2024-07-03 22:38:08 +0300

    Change LogoutCommand

--- Clerkgroup.AuthView/Commands/LogoutCommand.cs
+++ Clerkgroup.AuthView/Commands/LogoutCommand.cs
@@ -1,14 +1,20 @@
-﻿using Clerkgroup.Application.Stores;
+﻿using Clerkgroup.Application.Services.ApiService;
+using Clerkgroup.Application.Stores;
 using Clerkgroup.Shared.Commands;
 
 namespace Clerkgroup.AuthView.Commands
 {
-    public class LogoutCommand(UserStore userStore) : CommandBase
+    public class LogoutCommand(UserStore userStore, IApiService apiService) : AsyncCommandBase
     {
         private readonly UserStore _userStore = userStore;
-
-        public override void Execute(object? parameter)
+        private readonly IApiService _apiService = apiService;
+        
+        public override async Task ExecuteAsync(object parameter)
         {
+            CancellationToken cancellationToken = new CancellationTokenSource().Token;
+
+            await _apiService.LogoutAsync(cancellationToken);
+
             _userStore.CurrentUser = null;
         }
     }
--- Clerkgroup.AuthView/ViewModels/LogoutViewModel.cs
+++ Clerkgroup.AuthView/ViewModels/LogoutViewModel.cs
@@ -1,12 +1,13 @@
-﻿using Clerkgroup.Application.Stores;
+﻿using Clerkgroup.Application.Services.ApiService;
+using Clerkgroup.Application.Stores;
 using Clerkgroup.AuthView.Commands;
 using Clerkgroup.Shared;
 using System.Windows.Input;
 
 namespace Clerkgroup.AuthView.ViewModels
 {
-    public class LogoutViewModel(UserStore userStore) : ViewModelBase
+    public class LogoutViewModel(UserStore userStore, IApiService apiService) : ViewModelBase
     {
-        public ICommand LogoutCommand { get; } = new LogoutCommand(userStore);
+        public ICommand LogoutCommand { get; } = new LogoutCommand(userStore, apiService);
     }
 }
